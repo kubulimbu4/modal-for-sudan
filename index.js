76b907428a0566b83a194a100e46207928b22f69
Sudanrai88 <101595305+Sudanrai88@users.noreply.github.com>
2023-04-27 17:49:15 +0100

    Added another class closeModal2 for both the button and the image to close the 'modal' or whatever it is called.

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const modal= document.querySelector('#modal');
 const openModal = document.querySelector('.open-button');
 const closeModal = document.querySelector('.close-button');
+const closeModal2 = document.querySelector('.close-button2');
+
 
 openModal.addEventListener('click', () => {
   modal.showModal();
 })
 closeModal.addEventListener('click', () => {
+  modal.close();
+})
+closeModal2.addEventListener('click', () => {
   modal.close();
 })
